24d406d63bd3885365edfb1ba797c70baf48bfa7
Ronald-Proyectos <ronald.millan.2408@gmail.com>
2023-06-30 20:02:49 -0500

    terminando el primer diseño de la ecommerce

--- app/assets/js/mostrar-productos.js
+++ app/assets/js/mostrar-productos.js
@@ -1,12 +1,31 @@
-export default async function obtenerProductos(url){
+export default async function obtenerProductos(dbURL){
     try{
+        const containerCard = document.querySelectorAll(".products__cards")
 
-        const res = await fetch(url),
+        const res = await fetch(dbURL),
               json = await res.json()
 
         if(!res.ok) throw new Error(`${res.status}: los productos no se cargaron, intentelo mas tarde.`)
 
-        console.log(json)
+        containerCard.forEach((container) => {
+            let card = ""
+
+            json.forEach(({id, nombre, categoria, precio, url}) => {
+                if(container.dataset.category === categoria){
+                    card += `
+                    <div id="${id}" class="product__card">
+                        <figure class="product-img">
+                            <img src="${url}" alt="${nombre}">
+                        </figure>
+                        <p class="product-name">${nombre}</p>
+                        <p class="product-price">$ ${precio}</p>
+                        <a href="#" id="${id}" class="ver-producto">Ver producto</a>
+                    </div>
+                `
+                } 
+            }) 
+            container.innerHTML = card
+        }) 
 
     }catch(err){
         alert(err)
